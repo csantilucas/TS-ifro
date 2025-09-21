7bf5b398893224eb163b28ec0517047d30d68e81
csantilucas <lucascsanti2006@gmail.com>
2025-09-21 19:38:20 -0400

    atvData

--- atvPraticaData/index.ts
+++ atvPraticaData/index.ts
@@ -1,14 +1,21 @@
-//1) Dado uma data qualquer calcule quantos dias faltan para o natal
-//2) Dado una data qualquer formate a impressão no formato dd/mm/yyyy HH:mm:55
-//3) Dado uma data qualquer adicione 38 dias e imprima o resultado
-//4) Dado uma data qualquer diga en qual dia da semana cairá
-//5) Dado duas datas verifique se a primeira data é menor que a segunda
-//6) Crie uma função que recebe duas datas e calcule quantos dias de diferente ha entre ambas
-
-
 let dataInicio: Date = new Date('2025-02-02t20:03:23')
 let dataFim: Date = new Date('2025-06-02t00:00:00')
 let natal: Date = new Date(2025, 9, 25)
+
+//1) Dado uma data qualquer calcule quantos dias faltan para o natal
+    diferençaDiasNatal(dataInicio, natal)
+//2) Dado una data qualquer formate a impressão no formato dd/mm/yyyy HH:mm:55
+    formataData(dataInicio)
+//3) Dado uma data qualquer adicione 38 dias e imprima o resultado
+    somarDias(dataInicio)
+//4) Dado uma data qualquer diga en qual dia da semana cairá
+    diaSemana(dataInicio)
+//5) Dado duas datas verifique se a primeira data é menor que a segunda
+    dataMaior(dataInicio, dataFim)
+//6) Crie uma função que recebe duas datas e calcule quantos dias de diferente ha entre ambas
+    diferençaDias(dataInicio, dataFim)
+
+
 
 
 //1) Dado uma data qualquer calcule quantos dias faltan para o natal
@@ -16,9 +23,6 @@ function diferençaDiasNatal(dataInicio: Date, dataFim: Date) {
     let getime: number = (dataFim.getTime() - dataInicio.getTime())/(1000 * 3600 * 24)
     console.log(`São ${Math.round(getime)} dias ate o natal\n`)//calculo pra trasnforma o tempo em dias
 }
-diferençaDiasNatal(dataInicio, natal)
-
-
 
 //2) Dado una data qualquer formate a impressão no formato dd/mm/yyyy HH:mm:55
 function formataData(data: Date) {
@@ -33,8 +37,6 @@ function formataData(data: Date) {
 
 
 }
-formataData(dataInicio)
-
 
 //3) Dado uma data qualquer adicione 38 dias e imprima o resultado
 function somarDias(data: Date) {
@@ -42,8 +44,6 @@ function somarDias(data: Date) {
     let novadata: Date = new Date(data.getTime() + (dias * 24 * 60 * 60 * 1000))
     console.log('nova data:', novadata.toLocaleDateString('pt-br'), '\n')
 }
-somarDias(dataInicio)
-
 
 //4) Dado uma data qualquer diga en qual dia da semana cairá
 function diaSemana(data:Date) {
@@ -60,13 +60,22 @@ function diaSemana(data:Date) {
     console.log('o dia da semana é: ',day[data.getDay()],'\n')
 
 }
-diaSemana(dataInicio)
-
 
 //5) Dado duas datas verifique se a primeira data é menor que a segunda
-function dataMaior(data:Date){
-}
+function dataMaior(data1:Date, data2:Date){
 
+
+        let datac1 :number = data1.getTime()
+        let datac2 :number  = data2.getTime()
+        if (datac1 > datac2){
+            console.log(`a data ${data1.toLocaleDateString('pt-br')} é maior que ${data2.toLocaleDateString('pt-br')}\n `)
+        } else{
+            console.log(`a data ${data2.toLocaleDateString('pt-br')} é maior que ${data1.toLocaleDateString('pt-br')}\n `)
+
+        }
+        
+
+}
 
 //6) Crie uma função que recebe duas datas e calcule quantos dias de diferente ha entre ambas
 function diferençaDias(dataInicio: Date, dataFim: Date) {
@@ -75,4 +84,3 @@ function diferençaDias(dataInicio: Date, dataFim: Date) {
     let diferença: number = (dataFim.getTime() - dataInicio.getTime()) / (1000 * 3600 * 24) //calculo pra trasnforma o tempo de milesegundos em dias
     console.log(`A diferenaça de dias é: ${Math.round(diferença)}\n`)
 }
-diferençaDias(dataInicio, dataFim)
